4176833740b0e56cf35364b3a50df76a5623cd4f
Micah Frost <micah.frost@gmail.com>
2016-01-12 12:14:43 -0700

    Simplify Rails support to simple Railtie

--- lib/envforcer/rails.rb
+++ lib/envforcer/rails.rb
@@ -1,24 +1,5 @@
 require 'envforcer/environment'
 
-module Envforcer
-  class RailsEnvironment < Environment
-    def config_file
-      path = root.join('.envforcer.yml')
-
-      @config_file ||=
-        if File.exist?(path)
-          YAML.load_file(path)
-        else
-          fail 'No .envforcer.yml file found.'
-        end
-    end
-
-    def root
-      Rails.root || Pathname.new(ENV['RAILS_ROOT'] || Dir.pwd)
-    end
-  end
-
-  class Railtie < ::Rails::Railtie
-    config.after_initialize { RailsEnvironment.enforce }
-  end
+class Envforcer::Railtie < ::Rails::Railtie
+  config.after_initialize { Envforcer.call }
 end
